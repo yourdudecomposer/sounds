84ae7891ea1bf09ac476aec263da5b5f3b306ca7
Filipp Goriainov <barrios1991phil@gmail.com>
2022-11-06 21:57:54 +0400

    add major/minor maker

--- sounds.js
+++ sounds.js
@@ -1,34 +1,72 @@
 
 function startProgram() {
     //create notes
-    const notes = new Array(12);
-    startFrequency = 440;
+    let notes = new Array(13);
 
+
+    startFrequency = 220;
+
+    const patterns = {
+        wellTemperate: (i) => startFrequency * (2 ** (i / 12))
+    }
 
 
     for (let i = 0; i < notes.length; i++) {
         notes[i] = {
             name: i,
-            frequency: startFrequency * (2 ** (i / 12)),
+            frequency: patterns.wellTemperate(i),
         }
     }
 
+    
+
+    function makeMinorScale(arr){
+      arr.splice(1, 1) //1,4,6,9,11
+      arr.splice(3,1)
+      arr.splice(4,1)
+      arr.splice(6,1)
+      arr.splice(7,1)
+        return arr
+    }
+    function makeMajorScale(arr){
+        arr.splice(1,1) //1,2,4,5,6
+        arr.splice(2,1)
+        arr.splice(4,1)
+        arr.splice(5,1)
+        arr.splice(6,1)
+        return arr
+    }
+  
+ notes = makeMajorScale(notes)
+
+    console.log(notes)
+
 
     const context = new AudioContext()
-   
+
     const gainNode = context.createGain()
     gainNode.gain.setValueAtTime(0.01, context.currentTime);
     gainNode.connect(context.destination)
 
-
-
     const duration = 100;
 
+    setInterval(() => {
+        const random = Math.floor(Math.random() * notes.length - 1) + 1
+        const frequency = notes[random].frequency;
+
+        const oscillator = context.createOscillator();
+        oscillator.type = 'square';
+        oscillator.frequency.setValueAtTime(frequency, context.currentTime)
+        oscillator.connect(gainNode)
+
+        oscillator.start()
+        oscillator.stop((context.currentTime + duration / 1000 - duration / 10000))
+    }, duration);
 
 
     setInterval(() => {
 
-        const random0to11 = Math.floor(Math.random() * 11) + 1
+        const random0to11 = Math.floor(Math.random() * notes.length - 1) + 1
         const frequency = notes[random0to11].frequency;
 
         const oscillator = context.createOscillator();
@@ -36,11 +74,10 @@ function startProgram() {
         oscillator.frequency.setValueAtTime(frequency, context.currentTime)
         oscillator.connect(gainNode)
 
-
-      
         oscillator.start()
         oscillator.stop((context.currentTime + duration / 1000 - duration / 10000))
-    }, duration);
+    }, duration*3);
+
 
 
 
