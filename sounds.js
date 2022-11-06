4ffa7dcead606a3dc3ebbc8fbc9d9328024dd313
Filipp Goriainov <barrios1991phil@gmail.com>
2022-11-06 23:21:47 +0400

    make 2 voice

--- sounds.js
+++ sounds.js
@@ -1,82 +1,107 @@
 
 function startProgram() {
     //create notes
-    let notes = new Array(13);
+    let bassNotes = new Array(13);
+    let melodyNotes = new Array(25);
 
 
-    startFrequency = 220;
+    startBassFrequency = 110;
+    startMelodyFrequency = 220;
 
-    const patterns = {
-        wellTemperate: (i) => startFrequency * (2 ** (i / 12))
+    //create Bass notes
+    for (let i = 0; i < bassNotes.length; i++) {
+        bassNotes[i] = startBassFrequency * (2 ** (i / 12))
+    }
+
+    //create Melody notes
+    for (let i = 0; i < melodyNotes.length; i++) {
+        melodyNotes[i] = startMelodyFrequency * (2 ** (i / 12))
     }
 
 
-    for (let i = 0; i < notes.length; i++) {
-        notes[i] = {
-            name: i,
-            frequency: patterns.wellTemperate(i),
-        }
-    }
 
-    
+    function makeMinorScale(arr) {
+        arr.splice(1, 1) //1,3,4,6,7; 8,10,11,13,14  //1,4,6,9,11; 13,16,18,21,23
+        arr.splice(3, 1)
+        arr.splice(4, 1)
+        arr.splice(6, 1)
+        arr.splice(7, 1)
 
-    function makeMinorScale(arr){
-      arr.splice(1, 1) //1,4,6,9,11
-      arr.splice(3,1)
-      arr.splice(4,1)
-      arr.splice(6,1)
-      arr.splice(7,1)
+        arr.splice(8, 1)
+        arr.splice(10, 1)
+        arr.splice(11, 1)
+        arr.splice(13, 1)
+        arr.splice(14, 1)
         return arr
     }
-    function makeMajorScale(arr){
-        arr.splice(1,1) //1,2,4,5,6
-        arr.splice(2,1)
-        arr.splice(4,1)
-        arr.splice(5,1)
-        arr.splice(6,1)
+    function makeMajorScale(arr) {
+        arr.splice(1, 1) //1,2,4,5,6; 8,9,11,12,13  //1,3,6,8,10; 13,15,18,20,22
+        arr.splice(2, 1)
+        arr.splice(4, 1)
+        arr.splice(5, 1)
+        arr.splice(6, 1)
+
+        arr.splice(8, 1)
+        arr.splice(9, 1)
+        arr.splice(11, 1)
+        arr.splice(12, 1)
+        arr.splice(13, 1)
         return arr
     }
-  
- notes = makeMajorScale(notes)
 
-    console.log(notes)
+    bassNotes = makeMinorScale(bassNotes)
+    melodyNotes = makeMinorScale(melodyNotes)
+
+    console.log(bassNotes)
+    console.log(melodyNotes)
 
 
     const context = new AudioContext()
 
-    const gainNode = context.createGain()
-    gainNode.gain.setValueAtTime(0.01, context.currentTime);
-    gainNode.connect(context.destination)
+    const gainBassNode = context.createGain()
+    const gainMelodyNode = context.createGain()
 
-    const duration = 100;
+    gainBassNode.gain.setValueAtTime(0.1, context.currentTime);
+    gainMelodyNode.gain.setValueAtTime(0.1, context.currentTime);
 
+    gainBassNode.connect(context.destination)
+    gainMelodyNode.connect(context.destination)
+
+    const duration = 800;
+
+
+
+//Bass
     setInterval(() => {
-        const random = Math.floor(Math.random() * notes.length - 1) + 1
-        const frequency = notes[random].frequency;
+
+        const random = Math.floor(Math.random() * bassNotes.length - 1) + 1
 
         const oscillator = context.createOscillator();
         oscillator.type = 'square';
-        oscillator.frequency.setValueAtTime(frequency, context.currentTime)
-        oscillator.connect(gainNode)
+        oscillator.type = 'sawtooth';
+        oscillator.type = 'triangle';
+        oscillator.frequency.setValueAtTime(bassNotes[random], context.currentTime)
+        oscillator.connect(gainBassNode)
 
         oscillator.start()
         oscillator.stop((context.currentTime + duration / 1000 - duration / 10000))
     }, duration);
 
-
+//Melody
     setInterval(() => {
 
-        const random0to11 = Math.floor(Math.random() * notes.length - 1) + 1
-        const frequency = notes[random0to11].frequency;
+        const random = Math.floor(Math.random() * melodyNotes.length - 1) + 1
 
         const oscillator = context.createOscillator();
-        oscillator.type = 'square';
-        oscillator.frequency.setValueAtTime(frequency, context.currentTime)
-        oscillator.connect(gainNode)
+    oscillator.type = 'sine';
+        oscillator.frequency.setValueAtTime(melodyNotes[random], context.currentTime)
+        oscillator.connect(gainMelodyNode)
 
         oscillator.start()
-        oscillator.stop((context.currentTime + duration / 1000 - duration / 10000))
-    }, duration*3);
+        oscillator.stop((context.currentTime + duration / 1000/4 - duration / 10000/4))
+    }, duration/4);
+
+
 
 
 
@@ -125,3 +150,5 @@ document.body.addEventListener('click', () => {
     !isPlaying ? startProgram() : window.location.reload();;
     isPlaying = true;
 })
+
+
