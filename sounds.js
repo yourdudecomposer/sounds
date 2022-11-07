f2290e0deef7729d534ba9203534a0e3dbe0c803
Filipp Goriainov <barrios1991phil@gmail.com>
2022-11-07 12:00:58 +0400

    one voice, same note duration

--- sounds.js
+++ sounds.js
@@ -4,7 +4,7 @@ function startProgram() {
     let melodyNotes = new Array(12);
 
 
-    startMelodyFrequency = 659.2551138257398 / 2;
+    startMelodyFrequency = 329.6275569128699;
 
     //create Melody notes
     for (let i = 0; i < melodyNotes.length; i++) {
@@ -12,45 +12,43 @@ function startProgram() {
     }
 
 
-    console.log(melodyNotes)
+    console.table(melodyNotes)
 
 
     const context = new AudioContext()
 
     const gainMelodyNode = context.createGain()
-
     gainMelodyNode.gain.setValueAtTime(0.1, context.currentTime);
-
     gainMelodyNode.connect(context.destination)
 
     const duration = 200;
 
+const savedMelodyNotes = [...melodyNotes]
 
 
-    setInterval(() => {
+    var myFunction = function (divisor) {
 
-        const random = Math.floor(Math.random() * melodyNotes.length - 1) + 1
+        random = Math.floor(Math.random() * melodyNotes.length - 1) + 1;
+
 
         const oscillator = context.createOscillator();
-        oscillator.type = 'sine';
+        oscillator.type = 'triangle';
         oscillator.frequency.setValueAtTime(melodyNotes[random], context.currentTime)
         oscillator.connect(gainMelodyNode)
-
         oscillator.start()
-        oscillator.stop((context.currentTime + duration / 1000 - duration / 10000))
-    }, duration);
+        console.log(melodyNotes)
+        melodyNotes.length >1? melodyNotes.splice(random,1) : melodyNotes = [...savedMelodyNotes];
+
+   
+        oscillator.stop((context.currentTime + duration / 1000/divisor -duration / 10000/divisor))
+
+        setTimeout(()=>{myFunction(divisor)}, duration/divisor);
+    }
+
+    
 
 
-
-
-
-
-
-
-
-
-
-
+setTimeout(()=>{myFunction(1)}, 0);
 
 
 
@@ -91,3 +89,4 @@ document.body.addEventListener('click', () => {
 })
 
 
+// startProgram()
